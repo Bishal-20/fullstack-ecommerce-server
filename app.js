0bfa8249d157d8809ddc5134f692101416d52549
Bishal-20 <161943871+Bishal-20@users.noreply.github.com>
2026-05-12 16:13:50 +0530

    Update CORS origin for bazzarbuddyadmin

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ require('dotenv').config();
 const authJwt = require('./helper/jwt.js');
 app.use(cors({
   origin: [
-    "https://bazzarbuddyadmin.netlify.app",
+    "https://6a0300ffbce4cca6418ecded--bazzar-buddyadmin.netlify.app",
     "https://bazzarbuddystore.netlify.app",
     "http://localhost:3000"
   ],
